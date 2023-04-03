1adcb41076b55c26f796127a3996935691bae724
HIRWA Junior <iamhirwejr@gmail.com>
2023-04-03 15:55:28 +0300

    Correct task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""A Rectangle class Created by ."""
+"""A Rectangle class Created by @Jr Hirwa on Apr 4 ."""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """rectangle class to define a rectangular shape."""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
+        """Make another Rectangle.
         Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
+            width (int): new rectangle width.
+            height (int): new rectangle height.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
+        """Get and set the rectangle width."""
         return self.__width
 
     @width.setter
@@ -29,7 +29,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
+        """Get and set the rectangle height."""
         return self.__height
 
     @height.setter
