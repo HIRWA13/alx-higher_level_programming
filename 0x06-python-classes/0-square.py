a3002b37bf90ab834f17216c4cdc71fabd0399d5
HIRWA Junior <iamhirwejr@gmail.com>
2023-04-03 13:56:34 +0300

    correction of task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 3 23:31:12 2023
+@author: Jt Hirwa
+"""
+
 
 class Square:
-    """An empty class for defining a square
-    
+    """An empty class Square for defining a square
+
     Attributes:
         empty
     """
