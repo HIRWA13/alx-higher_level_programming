a21d2a6bb073801c643c98dfd3d7c4666886cfe3
HIRWA Junior <iamhirwejr@gmail.com>
2023-04-03 15:00:04 +0300

    correct 0

--- 0x06-python-classes/0x08-python-more_classes/0-rectangle.py
+++ 0x06-python-classes/0x08-python-more_classes/0-rectangle.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Creatted on Tuesday Apr 4 00:27:23 2023
+Created on Tuesday Apr 4 00:27:23 2023
 @author: Jr Hirwa
 """
 
 
 class Rectangle:
-    """class Rectangle that defines a rectangle
+    """Empty class Rectangle that defines a rectangle
 
     Attributes:
         empty
     """
+
     pass
