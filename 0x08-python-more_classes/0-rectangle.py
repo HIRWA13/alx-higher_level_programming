d898a16f770756cbdd7beb76095085302f011c29
HIRWA Junior <iamhirwejr@gmail.com>
2023-04-03 15:08:23 +0300

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Crested on Tue Apr 4 00:23:55 2023
+@author: Jr Hirwa
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle which is defining a rectangular shape
+
+    Attributes:
+        empty
+    """
+
+    pass
